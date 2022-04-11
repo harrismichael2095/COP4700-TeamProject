af565a43c186a634abbea7ba0fd215588d00873a
Michael Harris <45339433+harrismichael2095@users.noreply.github.com>
2022-04-11 03:08:23 -0400

    Update index.php
    added code to store admin ID so it can be displayed for RSO creation

--- Project/index.php
+++ Project/index.php
@@ -3,6 +3,7 @@
 require_once('connect.php');
 session_start(); // get stored values
 $_SESSION['userID'] = -1;   // -1 means user is not logged in
+$_SESSION['admin_id'] = -1;
 
 
 if (array_key_exists('register', $_POST)) {
@@ -18,12 +19,31 @@ if (array_key_exists('register', $_POST)) {
         while($row = $results->fetch_assoc()){
             echo($row['user_id'] );
             $_SESSION['userID'] = $row['user_id'];
+
             header("Location: http://localhost/Project/home");
             }
         }  
-}
 
+
+        //Save Admin ID like userID so user can use it so create new RSO
+        $user_id = $_SESSION["userID"];
+        $sql = "SELECT admin.admin_id FROM admin WHERE (admin.user_id = $user_id)";
+        $test = $mysqli->query($sql);                       
+        if($test == false){ echo ("This user is not an Admin yet");
+        }
+        else{
+        while($row = $test->fetch_assoc()){
+            echo($row['admin_id'] );
+            $_SESSION['admin_id'] = $row['admin_id'];
+            header("Location: http://localhost/Project/home");
+        }
+    }
+
+    
+}
 ?>
+
+
 <h1>Login</h1>
     <div class="content">
     <form method="post">
